6a7a0837eff84ecb4e921d414190a8014a7dbab3
jfhinchcliffe <hinchy@gmail.com>
2016-10-15 16:02:45 +1100

    Just got the Robot.turn method going

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,5 +1,33 @@
 class Robot
 
+  attr_reader :x, :y, :facing
+
+  def initialize(x,y,facing)
+    @x = x
+    @y = y
+    @facing = facing
+  end
+
+  def turn(direction)
+    rotate(direction)
+  end
 
 
+  private
+    def rotate(direction)
+      valid_facings = ["NORTH", "EAST", "SOUTH", "WEST"]
+      current_facing_location = valid_facings.index(@facing)
+      if direction == "LEFT"
+        current_facing_location -= 1
+      elsif direction == "RIGHT"
+        current_facing_location += 1
+      end
+      if current_facing_location < 0
+        @facing = valid_facings.last
+      elsif current_facing_location > 3
+        @facing = valid_facings.first
+      else
+        @facing = valid_facings[current_facing_location]
+      end
+    end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,10 +1,33 @@
 require 'robot'
 describe Robot do
 
-  describe Robot do
+  before(:all) do
+    @robot = Robot.new(0,0,"NORTH")
+  end
 
+  it "can initialise a robot" do
+    expect(@robot.x).to eq 0
+    expect(@robot.y).to eq 0
+    expect(@robot.facing).to eq "NORTH"
+  end
 
-
+  it "can turn left and right" do
+    @robot.turn("LEFT")
+    expect(@robot.facing).to eq "WEST"
+    @robot.turn("LEFT")
+    expect(@robot.facing).to eq "SOUTH"
+    @robot.turn("LEFT")
+    expect(@robot.facing).to eq "EAST"
+    @robot.turn("LEFT")
+    expect(@robot.facing).to eq "NORTH"
+    @robot.turn("RIGHT")
+    expect(@robot.facing).to eq "EAST"
+    @robot.turn("RIGHT")
+    expect(@robot.facing).to eq "SOUTH"
+    @robot.turn("RIGHT")
+    expect(@robot.facing).to eq "WEST"
+    @robot.turn("RIGHT")
+    expect(@robot.facing).to eq "NORTH"
   end
 
 
