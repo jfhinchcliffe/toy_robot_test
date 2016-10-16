a13b5fb46eba908d58b5877109335ce262365dde
jfhinchcliffe <hinchy@gmail.com>
2016-10-16 12:25:28 +1100

    Finished table.move method and tests

--- lib/table.rb
+++ lib/table.rb
@@ -9,6 +9,22 @@ class Table
     false
   end
 
+  def self.move(x,y,facing)
+    x_updated = x
+    y_updated = y
+    case facing
+    when "NORTH"
+      x_updated = x + 1 if valid_position?(x + 1, y)
+    when "SOUTH"
+      x_updated = x - 1 if valid_position?(x - 1, y)
+    when "EAST"
+      y_updated = y + 1 if valid_position?(x, y + 1)
+    when "WEST"
+      y_updated = y - 1 if valid_position?(x, y - 1)
+    end
+    return [x_updated, y_updated, facing]
+  end
+
 
 
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -15,4 +15,22 @@ describe Table do
     expect(result).to eq false
   end
 
+  it "moves the robot forward correctly based on faced direction" do
+    p = Table.move(0,0,"NORTH")
+    expect(p).to eq [1,0,"NORTH"]
+    p = Table.move(3,3,"SOUTH")
+    expect(p).to eq [2,3,"SOUTH"]
+    p = Table.move(0,5,"WEST")
+    expect(p).to eq [0,4,"WEST"]
+  end
+
+  it "does not allow the robot to move off the table" do
+    p = Table.move(0,0,"SOUTH")
+    expect(p).to eq [0,0,"SOUTH"]
+    p = Table.move(5,3,"NORTH")
+    expect(p).to eq [5,3,"NORTH"]
+    p = Table.move(0,0,"WEST")
+    expect(p).to eq [0,0,"WEST"]
+  end
+
 end
